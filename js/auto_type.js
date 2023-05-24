45f72fa73bb3311203fb202d00251221b9bba91c
Nathan Turkson <nathanturkson@gmail.com>
2023-05-24 21:52:38 +0000

    First Commit

---
+++ js/auto_type.js
@@ -0,0 +1,9 @@
+
+var options = {
+  strings: ["Swipe with confidence, find your perfect match.", "Discover, connect, and let love unfold."],
+  typeSpeed: 70,
+  backSpeed: 30,
+  loop: true,
+};
+
+var typed = new Typed(".slogan-app", options); 
